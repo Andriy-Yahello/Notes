16f205ec5c95adcb858ad7d49feb8e28bde8ae3c
Andriy Yahello <andrey.lv.west@gmail.com>
2017-03-26 22:17:40 +0300

    first

---
+++ notes/Notes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes
+{
+    internal static class FindAndReplaceManager
+    {
+        public static void FindNext(string str)
+        {
+            Finder finder = new Finder();
+            Console.WriteLine(finder.FindNext(str));
+        }
+    }
+
+    internal class Finder
+    {
+        public class Notes
+        {
+            private string scratch = "";
+            public string Scratch
+            {
+                get { return scratch; }
+                set {
+                    if (value != null)
+                     scratch += value + "\n"; // \n new line
+                }
+                
+            }
+            
+            
+            public void Note(string str)
+            {
+                Console.WriteLine("A note '{0}' is saved.", str);
+            }
+        }
+        
+        public string FindNext(string strang)
+        {
+            return "Searching for: " + strang;
+
+        }
+    }
+    
+    
+
+}
---
+++ notes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindAndReplaceManager.FindNext("Superman");
+            string a = Console.ReadLine();
+
+            Finder.Notes note = new Finder.Notes();
+            note.Note(a);//method
+
+            note.Scratch = "Nice feature";//Property
+            note.Scratch = "";//Property + ""
+            note.Scratch = "it works";//Property + "it works"
+            Console.WriteLine(note.Scratch);
+
+            Console.ReadKey();
+        }
+    }
+}
